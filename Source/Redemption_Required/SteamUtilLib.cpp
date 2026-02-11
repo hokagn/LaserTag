9a927846f9374afef0baf797e66753794f29f3f6
hokagn <184084617+hokagn@users.noreply.github.com>
2026-02-11 20:43:03 +0900

    Added MultiplayerLobby

---
+++ Source/Redemption_Required/SteamUtilLib.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SteamUtilLib.h"
+/*
+* use if needed
+#include "ThirdParty/Steamworks/Steamv161/sdk/public/steam/steam_api.h"
+
+void USteamUtilLib::OpenSteamOverlay(FString Type)
+{
+
+	if (SteamAPI_Init())
+	{
+		SteamFriends()->ActivateGameOverlay(TCHAR_TO_ANSI(*Type));
+	}
+}
+*/
---
+++ Source/Redemption_Required/SteamUtilLib.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SteamUtilLib.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDEMPTION_REQUIRED_API USteamUtilLib : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	//UFUNCTION(BlueprintCallable)
+	//void OpenSteamOverlay(FString Type);
+};
